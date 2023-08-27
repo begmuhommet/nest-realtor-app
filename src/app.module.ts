5590b085686d93802b8a4abdc8e43ff9f144cf36
Bega Jumayev <bega@ekta.io>
2023-08-27 12:17:21 +0800

    ♻️ Refactoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HomeModule } from './home/home.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, HomeModule],
   controllers: [AppController],
   providers: [AppService],
 })
